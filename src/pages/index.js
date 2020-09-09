d07164878f7e8142b1cab5050c4920432966edfa
Svetlana Shinkar <svetanek@gmail.com>
2020-09-08 23:49:35 -0400

    display markdown data in a blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,21 @@ import SEO from "../components/seo"
 export default ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <div>First blog post</div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <h1>First blog post</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <span>
+            {node.frontmatter.title} - {node.frontmatter.date}
+          </span>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </div>
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div> */}
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
